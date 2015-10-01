365fc3e232292d7bd82eff40a4fe1ef2ff650a05
Danny Hunkele <danny@hunkele.net>
2015-10-01 01:15:50 -0700

    edited search result section, used js to remove image and insert card without styling

--- scripts/pokemon.js
+++ scripts/pokemon.js
@@ -1,8 +1,31 @@
 $(function() {
 
 	$('#submitPokemon').click(function() {
-		alert($('#firstPokemon').val());
-	})
+		var firstPokemon = $('#firstPokemon').val();
+		console.log(firstPokemon);
+
+		$.ajax({
+			url: 'http://pokeapi.co/api/v1/pokedex/'
+		}).success(function(data) {
+			var fullList = data.objects[0].pokemon;
+			fullList.forEach(function(i) {
+				if (firstPokemon === i.name) {
+					console.log('For' + i.name + ' you want ' , i.resource_uri);
+					$('#placeholder').remove();
+					$('.pokemonCard').attr('id', 'searchResult');
+					$.ajax({
+						url: 'http://pokeapi.co/' + i.resource_uri
+					}).success(function(pokemon) {
+						console.log('Here:' + pokemon.name);
+						$('#pokemonName').html(pokemon.name);
+						$('#hp').html(pokemon.hp);
+						$('#stats').html(' Height: ' + pokemon.height + ' Weight: ' + pokemon.weight);
+						$('#info').html('Attack: ' + pokemon.attack + '<br>Defense: ' + pokemon.defense + '<br>Happiness: ' + pokemon.happiness + '<br>Species: ' + pokemon.species);
+					});
+				}
+			});
+		});
+	});
 
 	//This is how to generate a full list of the pokemon needed for user input
 
