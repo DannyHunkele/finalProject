b67782a8e3173caf291d9d6bfd5a86951ca71d92
Danny Hunkele <danny@hunkele.net>
2015-10-01 16:48:21 -0700

    I FINALLY got the image to pull into the card

--- scripts/pokemon.js
+++ scripts/pokemon.js
@@ -19,6 +19,17 @@ $(function() {
 						console.log('Here:' + pokemon.name);
 						$('#pokemonName').html(pokemon.name);
 						$('#hp').html(pokemon.hp + ' HP');
+						$('#cardImage').html(function() {
+							var spriteLink = pokemon.sprites[0].resource_uri;
+							console.log('is this it? ' + spriteLink);
+							$.ajax({
+								url: 'http://pokeapi.co' + spriteLink
+							}).success(function(pokePic) {
+								console.log('image link? ' + pokePic.image);
+								$('#cardImage').html('<img src=\'http://pokeapi.co' + pokePic.image + '\'>');
+								return '<img src=\'http://pokeapi.co' + pokePic.image + '\'>'
+							});
+						});
 						$('#stats').html(' Height: ' + pokemon.height + ' Weight: ' + pokemon.weight);
 						$('#info').html('Attack: ' + pokemon.attack + '<br>Defense: ' + pokemon.defense + '<br>Happiness: ' + pokemon.happiness + '<br>Species: ' + pokemon.species);
 					});
